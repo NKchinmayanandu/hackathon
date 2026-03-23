94b8d597b93d5cac2ea2c643752fad2fcaa24c22
NKchinmayanandu <nkchinmayanandunk@gmail.com>
2026-03-23 09:29:58 +0000

    decoding

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,29 +1,11 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List
-from pydantic import Field
-# inside schemas (same file is fine)
-
-from pydantic import BaseModel, Field
 from typing import Optional, List, Dict, Any
 
 class PredictRequest(BaseModel):
     query: Optional[str] = None
     data: Optional[Dict[str, Any]] = None
 
-# ── User Schemas ──────────────────────────────
-class UserCreate(BaseModel):
-    name: str
-    email: str
 
-class UserResponse(UserCreate):
-    id: int
-
-    posts: List["PostResponse"] = Field(default_factory=list)
-    class Config:
-        from_attributes = True
-
-
-# ── Post Schemas ──────────────────────────────
 class PostCreate(BaseModel):
     title: str
     content: str
@@ -37,5 +19,3 @@ class PostResponse(PostCreate):
         from_attributes = True
 
 
-# Needed for forward reference in UserResponse
-UserResponse.model_rebuild()
